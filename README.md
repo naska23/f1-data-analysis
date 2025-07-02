bab374032d6dec6d53049a2c0ad9e52e73694300
naska23 <naska.kvarantan@gmail.com>
2025-07-02 14:55:11 +0200

    race number trend graph creation

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 # f1-data-analysis
-## Today's Agenda
-### Practicing images!
+## Formula 1 Data Analysis (1950–2024)
+This project explores the evolution, performance trends, and race dynamics in the **Formula 1 World Championship** using historical data from 1950 through 2024. The analysis uses a dataset sourced from Kaggle and dives into a variety of exploratory, performance-based, geographic, and lap-level questions. 
+
+## Dataset
+
+- **Source**: [Formula 1 World Championship (1950 - 2024)](https://www.kaggle.com/datasets/rohanrao/formula-1-world-championship-1950-2020/data)
+- **Data Includes**: Drivers, races, circuits, constructors, lap times, pit stops, qualifying results, status (e.g., crashes), and more.
+
+## Project Aim
+
+The goal is to explore various aspects of F1 history and performance using real-world data. By answering targeted questions, the project highlights trends in driver performance, circuit characteristics, and changes in the sport over time.
+
+## Project Structure
+
+## Tools
+- **Languages**: Python and SQL
+- **Libraries**: pandas, matplotlib, seaborn, numpy, dplyr, matplotlib.pyplot, statistics
+- **Environments**: Pycharm, DB Browser for SQLite
+
+## Key Questions Explored
+
+1. How has the number of races per season evolved over time? [Evolution of F1 Race Counts per Season]()
+2. Which circuits have hosted the most races?
+3. Which drivers raced for the most teams during their careers?
+4. Which circuits have the best or worst pole-to-win conversion rate?
+
+
 ! [Pit Stop Trend By Circuit](images/pit_stop_trend_by_circuit.png)
---
+++ graphs_creation.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+
+def create_race_count_graph(df):
+    # extract columns
+    years = df['year']
+    num_races = df['num_races']
+
+    # create line chart
+    plt.figure(figsize=(18, 7), facecolor='black')
+    ax = plt.gca()
+    ax.set_facecolor('black')  # plot bg
+    plt.plot(years, num_races, marker='o', linestyle='-', color='#FF0000')
+
+    # add labels and title
+    plt.xlabel('Year', fontsize=14, fontweight='bold', color='white')
+    plt.ylabel('Number of Races', fontsize=14, fontweight='bold', color='white')
+    plt.title('Evolution Of F1 Race Counts Per Season', fontsize=20, fontweight='bold', color='white')
+    plt.grid(False)
+
+    # Set tick params to white
+    ax.tick_params(colors='white', which='both')  # tick labels and ticks
+
+    # force y-axis to use full integers
+    plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
+
+    # Manually set y ticks for better control
+    ymin, ymax = 6, max(num_races.max(), 25)
+    plt.yticks(range(ymin, ymax + 1, 2))  # ticks every 2 units: 6, 8, 10...
+
+    # set x-axis ticks and labels
+    start, end = 1950, 2024
+    ticks = list(range(start, end + 1, 2))
+    if ticks[-1] != end:
+        ticks.append(end)
+
+    plt.xticks(ticks=ticks, rotation=45, color='white')
+
+    # make x and y axis lines white
+    ax.spines['bottom'].set_color('white')
+    ax.spines['left'].set_color('white')
+
+    # show plot
+    plt.tight_layout()
+    plt.savefig('/Users/oblj-nkvarantan/PycharmProjects/F1 DATA/race_counts_year_graph.png', dpi=300)
+    print("Trend plot saved.")
+    plt.show()
+
+
+def main():
+    # load the race_counts_by_year.csv file
+    df = pd.read_csv('/Users/oblj-nkvarantan/PycharmProjects/F1 DATA/race_counts_by_year.csv')
+
+    # create plot
+    create_race_count_graph(df)
+
+
+if __name__ == "__main__":
+        main()
